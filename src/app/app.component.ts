36946ecea00214b466f722fa339a0ad5050ed7ff
Sabine Detering <sabine.detering@web.de>
2022-04-06 23:52:13 +0200

    header component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'sabinedetering.de';
-  images = ['laptop.jpg', 'tu-dortmund.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      },10)
-    }, 8000);
-  }
+export class AppComponent {
+  title = 'sabinedetering.de'; 
 }
